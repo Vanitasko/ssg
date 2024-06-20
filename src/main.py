3aa1f70707e9f38b15516ad4588fec1f33a7d397
Vanitas <pfboltryk@gmail.com>
2024-06-20 23:54:25 +0100

    Initialised SSG project

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+
+def main():
+    test_node = TextNode("some content", "some type", "some url")
+    print(test_node)
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,15 @@
+class TextNode:
+
+    def __init__(self, content, text_type, url):
+        self.content = content
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return
+        self.content == other.content \
+            and self.text_type == other.text_type \
+            and self.url == other.url
+
+    def __repr__(self):
+        return f"TextNode({self.content}, {self.text_type}, {self.url})"
